3c5f681b7396520dfc100718e42a9d44f9d0bc52
cyhaz <cyhaz@naver.com>
2020-03-20 12:09:57 +0900

    피자목록어댑터 / 리스트뷰 연결

--- app/src/main/java/kr/co/youhyun/a20200320_pizzastorelist/MainActivity.java
+++ app/src/main/java/kr/co/youhyun/a20200320_pizzastorelist/MainActivity.java
@@ -5,10 +5,17 @@ import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import kr.co.youhyun.a20200320_pizzastorelist.adapters.PizzaStoreAdapter;
 import kr.co.youhyun.a20200320_pizzastorelist.databinding.ActivityMainBinding;
+import kr.co.youhyun.a20200320_pizzastorelist.datas.PizzaStore;
 
 public class MainActivity extends BaseActivity {
 
+    List<PizzaStore> pizzaStores = new ArrayList<>();
+    PizzaStoreAdapter pizzaStoreAdapter = null;
     ActivityMainBinding binding = null;
 
     @Override
@@ -26,6 +33,9 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setValues() {
+
+        pizzaStoreAdapter = new PizzaStoreAdapter(mContext, R.layout.pizza_store_list_item, pizzaStores);
+        binding.pizzaListView.setAdapter();
 
     }
 }
