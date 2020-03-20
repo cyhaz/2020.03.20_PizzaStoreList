d4f04524916dcadd40d76df862e82d43b1732f73
cyhaz <cyhaz@naver.com>
2020-03-20 12:04:17 +0900

    피자가게 어댑터 뼈대 작업

---
+++ app/src/main/java/kr/co/youhyun/a20200320_pizzastorelist/adapters/PizzaStoreAdapter.java
@@ -0,0 +1,41 @@
+package kr.co.youhyun.a20200320_pizzastorelist.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+import kr.co.youhyun.a20200320_pizzastorelist.R;
+import kr.co.youhyun.a20200320_pizzastorelist.datas.PizzaStore;
+
+public class PizzaStoreAdapter extends ArrayAdapter<PizzaStore> {
+
+    Context mContext;
+    List<PizzaStore> mList;
+    LayoutInflater inf;
+
+    public PizzaStoreAdapter(@NonNull Context context, int resource, @NonNull List<PizzaStore> objects) {
+        super(context, resource, objects);
+
+        mContext = context;
+        mList = objects;
+        inf = LayoutInflater.from(mContext);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        View row = convertView;
+        if (row == null) {
+            row = inf.inflate(R.layout.pizza_store_list_item, null);
+        }
+        return row;
+    }
+}
